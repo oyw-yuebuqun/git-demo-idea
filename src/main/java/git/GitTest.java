79cdb26e57461abb48661332074bc33be48ae9fa
ouyaowen <ouyaowen0817@163.com>
2022-09-07 09:56:07 +0800

    second commit

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -2,6 +2,6 @@ package git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git222!");
     }
 }

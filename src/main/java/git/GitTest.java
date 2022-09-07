8994cece031eff9ca8481d0ce3e2b66415c6f92c
ouyaowen <ouyaowen0817@163.com>
2022-09-07 10:29:05 +0800

    hot-fix commit2

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -3,6 +3,6 @@ package git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git222!");
-        System.out.println("hot-fix");
+        System.out.println("hot-fix222");
     }
 }
